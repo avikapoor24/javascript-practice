91a60f00cbed4d026fb05896a0dc9d014558a437
Avi Kapoor <85234265+avikapoor24@users.noreply.github.com>
2024-07-05 18:15:00 +0000

    1st commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+let Name = "Avi Kapoor"
+let age = 22
+const dob = "16/04/2002"
+var petsName = "Shiro"
+console.table([Name,age,dob,petsName])
+
+//Changing the pet's name 
+var petsName = "Dobi"
+console.table([Name,age,dob,petsName])
+
+/*
+Prefer not to use var because of issue in 
+block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined＝>
+// symbol => unique
+
+// object
+
+"use strict"; // treats all Js code as newer version
+
+let age = 20
+let name = "Avi Kapoor"
+
+console.log(typeof 20)
+console.log(typeof name)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
+
+
